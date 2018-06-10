cd01e510fd18b889d8aa7161767fe0701e48af45
takaba <ec2-user@ip-172-31-34-107.ap-southeast-1.compute.internal>
2018-06-10 12:24:43 +0000

    Finish ch05

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -82,5 +82,6 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.include Rails.application.routes.url_helpers
   config.include Capybara::DSL
 end
